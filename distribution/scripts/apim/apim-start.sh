c94142aa13a187e182a125ea71ee96cd8a70fbb5
chamilaadhi <cka.adhi@gmail.com>
2019-12-20 15:39:38 +0530

    Add jdk 11 support

--- distribution/scripts/apim/apim-start.sh
+++ distribution/scripts/apim/apim-start.sh
@@ -52,7 +52,7 @@ if [[ -z $heap_size ]]; then
 fi
 
 jvm_dir=""
-for dir in /usr/lib/jvm/jdk1.8*; do
+for dir in /usr/lib/jvm/jdk*; do
     [ -d "${dir}" ] && jvm_dir="${dir}" && break
 done
 export JAVA_HOME="${jvm_dir}"
@@ -84,7 +84,9 @@ echo "Setting Heap to ${heap_size}"
 export JVM_MEM_OPTS="-Xms${heap_size} -Xmx${heap_size}"
 
 echo "Enabling GC Logs"
-export JAVA_OPTS="-XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:/home/ubuntu/wso2am/repository/logs/gc.log -XX:ErrorFile=/home/ubuntu/java_error.log"
+#export JAVA_OPTS="-XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:/home/ubuntu/wso2am/repository/logs/gc.log"
+export JAVA_OPTS="-Xlog:gc*,safepoint,gc+heap=trace:file=/home/ubuntu/wso2am/repository/logs/gc.log:uptime,utctime,level,tags "
+
 
 echo "Starting APIM"
 wso2am/bin/wso2server.sh start
